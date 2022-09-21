21dede8897680314670b36ef62d2f8829fba777f
Kristof Schepers <schepers.kristof@gmail.com>
2022-09-21 15:29:50 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const baseURL =
+  './sheet.svg';
+
+class MyIconElement extends HTMLElement {
+  static get observedAttributes() {
+    return ['name'];
+  }
+
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+    const template = document.getElementById('my-icon');
+    const clone = template.content.cloneNode(true);
+    this.shadowRoot.appendChild(clone);
+
+    this.useEl = this.shadowRoot.getElementById('use');
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.useEl.setAttribute('xlink:href', `${baseURL}#icon-${newValue}`);
+  }
+}
+
+customElements.define('my-icon', MyIconElement);
